090e5f160958e1f4ccccf9ddc107514d7dab6180
Shaan Mathur <shaanm2@uci.edu>
2023-09-25 15:30:30 -0700

    Added all texting functionality, so the program fully works now.

--- text.py
+++ text.py
@@ -1,2 +1,17 @@
+from twilio.rest import Client
+
+
+account_sid = 'YOUR_TWILIO_ACCOUNT_SID'
+auth_token = 'YOUR_TWILIO_AUTH_TOKEN'
+twilio_phone_number = 'YOUR_TWILIO_PHONE_NUMBER'
+your_phone_number = 'YOUR_PHONE_NUMBER_TO_RECEIVE_NOTIFICATIONS'
+
 def send_text(new_items):
-    print(f"New Item: {new_items}")
+    client = Client(account_sid, auth_token)
+    for key in new_items.keys():
+        message_body = f'New item on sale: {key}\nCheck it out: {new_items[key]}'
+        message = client.messages.create(
+            body = message_body,
+            from_ = twilio_phone_number,
+            to = your_phone_number
+        )
